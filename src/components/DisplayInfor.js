942a7f0b46b6fabfde7dfc58754fccd891ea6889
Tong Ngoc Khanh <khanhtongngoc.haui@gmail.com>
2024-12-10 15:47:15 +0700

    react

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -4,18 +4,36 @@ import './DisplayInfor.scss';
 import logo from './../logo.svg';
 
 class DisplayInfor extends React.Component {
-    state = {
-        isShowListUser: true
+    constructor(props) {
+        console.log('>>> call me constructor:1')
+        super(props);
+        this.state = {
+            isShowListUser: true
+        }
     }
+    componentDidMount() {
+        console.log('>>> call me component did mount')
+        setTimeout(() => {
+            document.title = 'Trang Cua Tong Ngoc Khanh'
+        }, 3000)
+    }
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('>>> call me component did update', this.props, prevProps)
+        if (this.props.listUsers !== prevProps.listUsers) {
+            if (this.props.listUsers.length === 5) {
+                alert('you have got 5 member');
+            }
+        }
+    }
+
     handleShowHide = () => {
         this.setState({
             isShowListUser: !this.state.isShowListUser
         })
     }
     render() {
-
+        console.log('>>>call me render ')
         const { listUsers } = this.props;
-        console.log(listUsers);
         return (
             <div className="display-infor-container">
                 {/* <img src={logo} /> */}
