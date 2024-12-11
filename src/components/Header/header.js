005e4aeb309096248207cf4791b1e91f3dd89e40
Tong Ngoc Khanh <khanhtongngoc.haui@gmail.com>
2024-12-11 11:15:34 +0700

    react

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -2,26 +2,24 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 const Header = () => {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand href="#home">GABOTABO</Navbar.Brand>
+                <NavLink to='/' className='navbar-brand'>GABOTABO</NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Link to='/' className='nav-link'>Home</Link>
-                        <Link to='/users' className='nav-link'>User</Link>
-                        <Link to='/admin' className='nav-link'>admin</Link>
+                        <NavLink to='/' className='nav-link'>Home</NavLink>
+                        <NavLink to='/users' className='nav-link'>User</NavLink>
+                        <NavLink to='/admin' className='nav-link'>Admin</NavLink>
                     </Nav>
                     <Nav>
                         <NavDropdown title="Setting" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Log In</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Log Out
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.1">Profile</NavDropdown.Item>
+                            <NavDropdown.Item >Log In</NavDropdown.Item>
+                            <NavDropdown.Item >Log Out</NavDropdown.Item>
+                            <NavDropdown.Item >Profile</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
