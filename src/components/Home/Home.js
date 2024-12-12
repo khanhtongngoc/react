4de1e66f3b3ae1340ec3cf0605a928a005b3023e
Tong Ngoc Khanh <khanhtongngoc.haui@gmail.com>
2024-12-12 10:30:51 +0700

    react

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,31 @@
+import videoHomepage from '../../assets/video-hompage.mp4';
 const HomePage = (props) => {
     return (
-        <div className="homepage-container">
-            Home Page
+        <div className='container'>
+            <div className="homepage-container">
+                {/* {loop autoplay playsinline="" x-webkit-airplay="deny" muted} */}
+                <div className='homepage-video'>
+                    <video autoPlay loop muted>
+                        <source
+                            src={videoHomepage}
+                            type="video/mp4"
+                        />
+                    </video>
+                </div>
+                <div className='homepage-content'>
+                    <div>
+
+                        <h1 className='title-1'>Get to know your customers with forms worth filling out</h1>
+
+                    </div>
+                    <div>
+                        <p className='title-2'>Collect all the data you need to understand customers with forms designed to be refreshingly different.</p>
+                    </div>
+                    <div>
+                        <button className='title-3'>Get started-it's free</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
